babefce4eecf0ccde6d4e9caee17e9999ef79a16
Sriram Kakarala <sriram@promobitech.com>
2019-03-17 15:23:09 +0530

    Generating crypto assets.

---
+++ generate_crypto_assets.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+CHANNEL_NAME=realdirectchannel
+
+# remove previous crypto material and config transactions
+rm -fr config/*
+rm -fr crypto-config/*
+
+# generate crypto material
+cryptogen generate --config=./crypto-config.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  exit 1
+fi
+
+# generate genesis block for orderer
+configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
+# TODO: What does this do?
+# generate anchor peer transaction
+configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/RealDirectNSDLanchors.tx -channelID $CHANNEL_NAME -asOrg RealDirectNSDL
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for RealDirectNSDL..."
+  exit 1
+fi
